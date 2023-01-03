d435e292c23a2bc66650cb61411b6cd5c1292a1c
rw-a <rwa2684@gmail.com>
2023-01-04 09:52:16 +1000

    init

---
+++ signin/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+from django.utils import timezone
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Full Name")
+    emergency_contact = models.CharField(max_length=10, validators=[MinLengthValidator(4)],
+                                         help_text="Phone number should be of a friend/relative, "
+                                                   "rather than the person themself.")
+    media_permission = models.BooleanField(default=False)
+    date_added = models.DateTimeField(default=timezone.now)
+
+
+class Signin(models.Model):
+    is_signin = models.BooleanField(choices=[(True, "Sign in"), (False, "Sign out")], verbose_name="Sign in/out")
+    date = models.DateTimeField(default=timezone.now)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Sign in/out"
