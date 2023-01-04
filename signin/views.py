f4db4d7e55fe3bf1f92291fe0ec9892996711256
rw-a <rwa2684@gmail.com>
2023-01-04 10:57:56 +1000

    added qrscanner to signin.html

--- signin/views.py
+++ signin/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 
 
+@staff_member_required
 def signin_page(request):
     context = {"is_signin": True}
     return render(request, 'signin/signin.html', context)
 
 
+@staff_member_required
 def signout_page(request):
     context = {"is_signin": False}
     return render(request, 'signin/signin.html', context)
