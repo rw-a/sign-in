1a2c9c07d74e6fb417148d6e66595519a99a673b
rw-a <rwa2684@gmail.com>
2023-01-04 17:27:44 +1000

    added sign in request to server on scan

--- signin/urls.py
+++ signin/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'signin'
 urlpatterns = [
     path('signin/', views.signin_page, name='signin'),
-    path('signout/', views.signout_page, name='signout')
+    path('signout/', views.signout_page, name='signout'),
+    path('api/signin', views.signin_request, name='signin_request')
 ]
--- signin/views.py
+++ signin/views.py
@@ -1,5 +1,8 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.contrib.admin.views.decorators import staff_member_required
+from .models import Person, Signin
 
 
 @staff_member_required
@@ -12,3 +15,16 @@ def signin_page(request):
 def signout_page(request):
     context = {"is_signin": False}
     return render(request, 'signin/signin.html', context)
+
+
+@staff_member_required
+def signin_request(request):    # this is also handles signout requests
+    if request.method == "POST":
+        data = json.loads(request.body)
+        try:
+            person = Person.objects.get(pk=data['pk'])
+        except Person.DoesNotExist:
+            return JsonResponse({"success": "false"})
+        signin = Signin(is_signin=data['is_signin'], person=person)
+        signin.save()
+        return JsonResponse({"success": "true", "person": person.name})
