35167e33666ee2251f3602fc7107d22ece635219
rw-a <rwa2684@gmail.com>
2023-01-05 21:54:42 +1000

    moved navbar to separate template

--- signin/urls.py
+++ signin/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'signin'
 urlpatterns = [
-    path('signin/', views.qr_signin_page, name='signin'),
-    path('signout/', views.qr_signout_page, name='signout'),
+    path('signin/', views.signin_page, name='signin'),
+    path('signout/', views.signout_page, name='signout'),
     path('qrsignin/', views.qr_signin_page, name='qrsignin'),
     path('qrsignout/', views.qr_signout_page, name='qrsignout'),
     path('api/signin', views.signin_request, name='signin_request')
--- signin/views.py
+++ signin/views.py
@@ -5,15 +5,42 @@ from django.contrib.admin.views.decorators import staff_member_required
 from .models import Person, Signin
 
 
+def get_signedin_people(signed_in: bool):
+    # signed_in determines if it gets signed in people (True) or signed out people (False)
+    people = {}     # dict with pk as key and name as value
+    for person in Person.objects.all():
+        if person.signin_set.count() == 0:
+            # if a person has no sign ins, treat them as signed out
+            if not signed_in:
+                people[person.pk] = person.name
+        else:
+            last_signin = person.signin_set.latest("date")  # gets the most recent sign in/out
+            if last_signin.is_signin == signed_in:
+                people[person.pk] = person.name
+    return people
+
+
+@staff_member_required
+def signin_page(request):
+    context = {"page": "signin", "is_signin": True}
+    return render(request, 'signin/signin.html', context)
+
+
+@staff_member_required
+def signout_page(request):
+    context = {"page": "signout", "is_signin": False}
+    return render(request, 'signin/signin.html', context)
+
+
 @staff_member_required
 def qr_signin_page(request):
-    context = {"is_signin": True}
+    context = {"page": "qrsignin", "is_signin": True}
     return render(request, 'signin/qrsignin.html', context)
 
 
 @staff_member_required
 def qr_signout_page(request):
-    context = {"is_signin": False}
+    context = {"page": "qrsignout", "is_signin": False}
     return render(request, 'signin/qrsignin.html', context)
 
 
