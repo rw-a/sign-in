69a2d8c21c191d3bab9bfc7c171d0a8af11e4655
rw-a <rwa2684@gmail.com>
2023-01-27 22:31:57 +1000

    improved input processing and added media permission field

--- signin/management/commands/import_csv.py
+++ signin/management/commands/import_csv.py
@@ -14,19 +14,45 @@ class Command(BaseCommand):
         try:
             with open(options['filename'], 'r', encoding='utf-8-sig') as file:
                 reader = csv.DictReader(file)
-                people = []
+
+                people_added = []
+                people_existed = []
+
                 for row in reader:
-                    people.append(row["name"])
-                    person = Person(name=row["name"],
-                                    emergency_contact_name=row["emergency_contact_name"],
-                                    emergency_contact_phone_number=row["emergency_contact_phone_number"])
+                    # strip inputs (remove trailing whitespaces on start and end)
+                    name = row["name"].strip()
+                    emergency_contact_name = row["emergency_contact_name"].strip()
+                    emergency_contact_phone_number = row["emergency_contact_phone_number"].strip()
+
+                    # don't do anything if the person already exists
+                    if Person.objects.filter(name=name).exists():
+                        people_existed.append(name)
+                        continue
+
+                    if len(emergency_contact_phone_number) > 0 and emergency_contact_phone_number[0] == '4':
+                        emergency_contact_phone_number = '0' + emergency_contact_phone_number
+
+                    person = Person(
+                        name=name,
+                        emergency_contact_name=emergency_contact_name,
+                        emergency_contact_phone_number=emergency_contact_phone_number
+                    )
+
+                    if row["media_permission"].lower() in ['yes', 'true']:
+                        person.media_permission = True
+
                     person.save()
-            self.stdout.write(self.style.SUCCESS(f"Successfully loaded {options['filename']}. "
-                                                 f"The following people were added:\n{', '.join(people)}"))
+                    people_added.append(name)
+
+            self.stdout.write(self.style.SUCCESS(f"Successfully loaded {options['filename']}.\n\n"
+                                                 f"The following people were added:\n{', '.join(people_added)}\n"))
+            self.stdout.write(self.style.NOTICE(f"The following people already existed, so nothing was changed:"
+                                                f"\n{', '.join(people_existed)}"))
         except FileNotFoundError:
             with open(options['filename'], 'w') as file:
                 writer = csv.DictWriter(file,
-                                        fieldnames=["name", "emergency_contact_name", "emergency_contact_phone_number"])
+                                        fieldnames=["name", "emergency_contact_name", "emergency_contact_phone_number",
+                                                    "media_permission"])
                 writer.writeheader()
             self.stdout.write(self.style.WARNING(f"{options['filename']} not found, so an empty CSV with that name "
                                                  f"was created."))
