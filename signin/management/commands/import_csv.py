0359a9e6be4f575d5491edf698068887965df23b
rw-a <rwa2684@gmail.com>
2023-01-27 22:08:11 +1000

    fixed utf8 signed

--- signin/management/commands/import_csv.py
+++ signin/management/commands/import_csv.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            with open(options['filename']) as file:
+            with open(options['filename'], 'r', encoding='utf-8-sig') as file:
                 reader = csv.DictReader(file)
                 people = []
                 for row in reader:
