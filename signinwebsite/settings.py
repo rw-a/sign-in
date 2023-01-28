4f1a0ad2ffacd8cd412cfbb0fb0ff343d481a5c5
rw-a <rwa2684@gmail.com>
2023-01-28 12:10:15 +1000

    update settings.py

--- signinwebsite/settings.py
+++ signinwebsite/settings.py
@@ -28,9 +28,9 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
