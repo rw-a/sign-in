f7ad1ac9394e132717b276e719f2721d3f47f6b0
rw-a <rwa2684@gmail.com>
2024-02-04 15:50:45 +1000

    refactor: move js to static file

---
+++ signin/static/signin.js
@@ -0,0 +1,44 @@
+function signIn(element) {
+    const is_signin = element.checked;
+
+    fetch(api_signin_path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "X-CSRFToken": csrf_token
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify({
+            pk: element.value,
+            is_signin: is_signin,
+            session: current_session_code
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+        updatePeopleCount();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function updatePeopleCount() {
+    const numPeople = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    if (numPeople > 0) {
+        document.getElementById('peopleCount').innerText = `Signed In: ${numPeople}`;
+    } else {
+        document.getElementById('peopleCount').innerText = "Nobody is signed in right now";
+    }
+}
+updatePeopleCount();
+
+// Load font size from options
+const fontSize = window.localStorage.getItem("fontSize") || "16";
+document.querySelector('html').style.fontSize = `${fontSize}px`;
+
+// Auto-refresh the page every 5 seconds
+setTimeout(() => {
+    location.reload();
+}, 5000);
